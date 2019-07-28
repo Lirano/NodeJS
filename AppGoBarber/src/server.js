30b02f26ee06a4885d88aba7eb34472bb466f2b9
Lincoln Irano <lincoln.irano@hotmail.com>
2019-07-28 12:52:48 -0300

    Third COMMIT, AppGoBarber

--- AppGoBarber/src/server.js
+++ AppGoBarber/src/server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const nunjucks = require('nunjucks')
+const path = require('path')
 
 class App {
   constructor () {
@@ -6,11 +8,24 @@ class App {
     this.isDev = process.env.NODE_ENV !== 'production'
 
     this.middlewares()
+    this.views()
     this.routes()
   }
 
   middlewares () {
-    this.express.use(express.json())
+    this.express.use(express.urlencoded({ extended: false }))
+  }
+
+  views (){
+    nunjucks.configure(path.resolve(__dirname, 'app', 'views'), {
+      autoescape: true, // Faz autoscape dos arquivos automaticamente
+      express: this.express, // Recebe a variavel que tem o servidor express
+      watch: this.isDev // Faz com que todas as alteracoes em arquivos NUNJUCKS funcionem como o nodemon
+    });
+
+    this.express.use(express.static(path.resolve(__dirname, 'public')))
+    this.express.set('view engine', 'njk')
+
   }
 
   routes () {
